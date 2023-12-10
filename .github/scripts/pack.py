5a3a3cfd3cb92c3d34eda4ded6a1907e71b3dd98
miozune <miozune@gmail.com>
2023-12-10 18:22:31 +0900

    first commit

---
+++ .github/scripts/pack.py
@@ -0,0 +1,35 @@
+import shutil
+from os import path
+
+paths_to_be_packed = [
+    'config',
+    'resources',
+    'GregTech.lang',
+    'GregTech_US.lang',
+    'README.md',
+]
+
+to_be_renamed = {
+    'resources': 'config/txloader/forceload',
+}
+
+base_repo_path = 'repo'
+base_temp_path = 'temp'
+
+
+def copy_item(src_path, dest_path):
+    if path.isdir(src_path):
+        shutil.copytree(src_path, dest_path)
+    else:
+        shutil.copy(src_path, dest_path)
+
+
+def main():
+    for p in paths_to_be_packed:
+        src_path = path.join(base_repo_path, p)
+        dest_path = path.join(base_temp_path, to_be_renamed.get(p, p))
+        copy_item(src_path, dest_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,25 @@
+# GTNH-Japanese-Translation
+
+## GT New Horizonsの日本語化パック
+
+2023-12-04
+
+初版公開
+
+## 使い方
+
+1、Releasesから対応するバージョンを探す
+
+2、ダウンロード・解凍し、.minecraftフォルダに展開する
+
+## クレジット
+
+このプロジェクトは[GTNHの中国語翻訳チーム](https://github.com/Kiwi233/Translation-of-GTNH)からスクリプトをお借りしています。
+
+## ライセンス
+
+翻訳文はCC-BY-NC-SA 4.0の下で公開されます。その他スクリプト等はMIT Licenseです。
+
+## 貢献方法
+
+Paratranzで翻訳作業を行っています。どなたでも歓迎です！
